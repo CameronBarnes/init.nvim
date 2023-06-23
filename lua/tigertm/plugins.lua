c53a35ffdf906ef96a504324676bdaaf3924017f
Auriel Konungr <auriel@ares>
2023-06-22 17:06:16 -0700

    Added additional plugins

--- lua/tigertm/plugins.lua
+++ lua/tigertm/plugins.lua
@@ -213,5 +213,23 @@ local plugins = {
             },
         },
     },
+	{ "tenxsoydev/tabs-vs-spaces.nvim", config = true },
+	{
+		"Dhanus3133/LeetBuddy.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		config = function()
+			require("leetbuddy").setup({})
+		end,
+		keys = {
+			{ "<leader>lq", "<cmd>LBQuestions<cr>", desc = "List Questions" },
+			{ "<leader>ll", "<cmd>LBQuestion<cr>", desc = "View Question" },
+			{ "<leader>lr", "<cmd>LBReset<cr>", desc = "Reset Code" },
+			{ "<leader>lt", "<cmd>LBTest<cr>", desc = "Run Code" },
+			{ "<leader>ls", "<cmd>LBSubmit<cr>", desc = "Submit Code" },
+		},
+	},
 }
 return plugins
