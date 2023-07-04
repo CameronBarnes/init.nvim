1310649b4c443c2f965f0ca843d081d3fd314580
Cameron Barnes <cameron_barnes@outlook.com>
2023-07-04 04:05:32 -0700

    Trying to get a keybind for switching indentation from spaces to tabs, not currently working

--- lua/tigertm/keymap/vim.lua
+++ lua/tigertm/keymap/vim.lua
@@ -2,3 +2,10 @@ local wk = require("which-key")
 
 -- Previous had a keybinding here for the default nvim file browser,
 -- but we're not using that because we're using nvim-tree instead
+
+wk.register({
+	["<leader>V"] = {
+		name = "+Vim",
+		t = {vim.cmd(":set noet|retab!"), "Switch From Spaces to Tabs"}, -- For some reason this isnt working, I dont know why
+	}
+})
